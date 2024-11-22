e26097936a620d2109cd50868532f86d64d6fd21
Manar-Fuqha <manar0fuqha2002@gmail.com>
2024-11-22 21:38:52 +0300

    Add project files.

---
+++ SocialAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
